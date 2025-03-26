e98c2c7ede1f005da41029a5fd59641dccde8422
gepcel <gepcelway@gmail.com>
2025-03-26 16:47:54 +0800

    first commit

---
+++ LaunchCmd/LaunchCmd/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Diagnostics;
+
+namespace LaunchCmd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 1)
+            {
+                string inputpath = args[0];
+                var v = File.GetAttributes(inputpath);
+                if(v.HasFlag(FileAttributes.Directory))
+                {
+                    Process.Start("cmd.exe", "/k cd " + inputpath);
+                }
+                else
+                {
+                    Process.Start("cmd.exe", "/k cd " + Path.GetDirectoryName(inputpath));
+                }
+            }
+        }
+    }
+}
---
+++ LaunchCmdCpp/LaunchCmdCpp/LaunchCmdCpp.cpp
@@ -0,0 +1,58 @@
+﻿#include <windows.h>
+#include <string>
+#include <iostream>
+#include <filesystem>
+
+
+namespace fs = std::filesystem;
+
+int main(int argc, char* argv[])
+{
+    if (argc == 2)
+    {
+        std::string inputPath = argv[1];
+        DWORD fileAttributes = GetFileAttributesA(inputPath.c_str());
+
+        if (fileAttributes != INVALID_FILE_ATTRIBUTES)
+        {
+            std::string command;
+            if (fileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+            {
+                command = "cmd.exe /k cd \"" + inputPath + "\"";
+            }
+            else
+            {
+                fs::path filePath(inputPath);
+                command = "cmd.exe /k cd \"" + filePath.parent_path().string() + "\"";
+            }
+
+            STARTUPINFOA si = { sizeof(STARTUPINFOA) };
+            PROCESS_INFORMATION pi;
+            CreateProcessA(
+                NULL,
+                const_cast<char*>(command.c_str()),
+                NULL,
+                NULL,
+                FALSE,
+                0,
+                NULL,
+                NULL,
+                &si,
+                &pi
+            );
+
+            CloseHandle(pi.hProcess);
+            CloseHandle(pi.hThread);
+        }
+        else
+        {
+            std::cerr << "Invalid path: " << inputPath << std::endl;
+        }
+    }
+    else
+    {
+        std::cerr << "Usage: LaunchCmd <path>" << std::endl;
+    }
+
+    return 0;
+}
